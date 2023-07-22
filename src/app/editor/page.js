51cb5d60a7fe51f63aa94a32fc7e1ae158860d3e
dorplaut <dorplaut@gmail.com>
2023-07-22 05:24:12 +0300

    Update page.js

--- src/app/editor/page.js
+++ src/app/editor/page.js
@@ -2,17 +2,13 @@ import React from 'react';
 import Timeline from '@/components/Timeline';
 import axios from 'axios';
 import Editor from '@/components/Editor';
-import getTimeline from '@/utils/getTimeline';
+// import getTimeline from '@/utils/getTimeline';
 
-const page = async () => {
+const page = () => {
   // fetch data
-  const data = await getTimeline();
+  // const data = await getTimeline();
 
-  return (
-    <div>
-      <Editor data={data} />
-    </div>
-  );
+  return <div>{/* <Editor data={data} /> */}</div>;
 };
 
 export default page;
