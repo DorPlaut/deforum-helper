3fe1556690d787fa70dbc4ec8d47960393f37b7b
dorplaut <dorplaut@gmail.com>
2023-07-22 21:04:33 +0300

    Update page.js

--- src/app/editor/page.js
+++ src/app/editor/page.js
@@ -4,10 +4,8 @@ import Timeline from '@/components/Timeline';
 import axios from 'axios';
 import Editor from '@/components/Editor';
 import Form from '@/components/Form';
-import { useFramesStore } from '@/store/framesStore';
 
 const page = () => {
-  const { fps, frameCount } = useFramesStore((state) => state);
   return (
     <div>
       <Form />
