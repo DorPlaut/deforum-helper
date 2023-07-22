6db1c206fe7622702184c2a4b6e57e9de3a422fa
dorplaut <dorplaut@gmail.com>
2023-07-22 05:20:05 +0300

    fix dir

--- src/app/editor/page.js
+++ src/app/editor/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Timeline from './components/Timeline';
+import Timeline from '@/components/Timeline';
 import axios from 'axios';
-import Editor from './components/Editor';
+import Editor from '@/components/Editor';
 import getTimeline from '@/utils/getTimeline';
 
 const page = async () => {
