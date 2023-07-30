8263da57d952dfb7dbac29a48274abd764ac590e
dorplaut <dorplaut@gmail.com>
2023-07-31 02:18:40 +0300

    feat: download settings

--- src/app/livedemo/Scene.js
+++ src/app/livedemo/Scene.js
@@ -94,12 +94,12 @@ const Scene = () => {
       ]);
       populateBoxes();
       // console.log(cameraPosition);
-      cameraRef.current.position.x -= positionX / 25;
-      cameraRef.current.position.y += positionY / 25;
-      cameraRef.current.position.z -= positionZ / 25;
-      cameraRef.current.rotation.x += rotationX / 200;
-      cameraRef.current.rotation.y -= rotationY / 200;
-      cameraRef.current.rotation.z -= rotationZ / 200;
+      cameraRef.current.position.x -= positionX / 41;
+      cameraRef.current.position.y += positionY / 41;
+      cameraRef.current.position.z -= positionZ / 41;
+      cameraRef.current.rotation.x += rotationX / 500;
+      cameraRef.current.rotation.y -= rotationY / 500;
+      cameraRef.current.rotation.z -= rotationZ / 500;
     }
   });
 
